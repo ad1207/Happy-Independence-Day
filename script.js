822558798840e813fd235db49c77b3f60c450fa5
Adithya R <59307022+ad1207@users.noreply.github.com>
2022-01-25 12:20:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var x = document.getElementById("content")
+
+
+        setTimeout(() => {
+            x.innerHTML = "10";
+            setTimeout(() => {
+                x.innerHTML = "9";
+                setTimeout(() => {
+                    x.innerHTML = "8";
+                    setTimeout(() => {
+                        x.innerHTML = "7";
+                        setTimeout(() => {
+                            x.innerHTML = "6";
+                            setTimeout(() => {
+                                x.innerHTML = "5";
+                                setTimeout(() => {
+                                    x.innerHTML = "4";
+                                    setTimeout(() => {
+                                        x.innerHTML = "3";
+                                        setTimeout(() => {
+                                            x.innerHTML = "2";
+                                            setTimeout(() => {
+                                                x.innerHTML = "1";
+                                                setTimeout(() => {
+                                                    x.innerHTML = "Happy Independence Day"
+                                                }, 1000)
+                                            }, 1000)
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
